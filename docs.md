d9c399238ff430d9281ef6e84678923254731141
unknown <ans.ballard@gmail.com>
2015-07-01 10:40:04 -0500

    renamed docs, added some unit tests, couple more to go

---
+++ docs.md
@@ -0,0 +1,156 @@
+## `dateRange`
+
+
+
+### Parameters
+
+* `dateList` **`Array`** An array of strings representing dates  Throws an exception if invalid dates are given
+
+
+
+
+
+## `dateRange.getEndDate`
+
+Get the end date as an object
+
+
+
+
+Returns `Date` End date
+
+
+## `dateRange.getEndDate`
+
+Get a list of objects with .valid and .date members
+
+
+
+
+Returns `Array` List of dates with validity
+
+
+## `dateRange.getEndDateIndex`
+
+Get the end date index in the full list
+
+
+
+
+Returns `integer` End date index
+
+
+## `dateRange.getList`
+
+Get the list of dates as objects
+
+
+
+
+Returns `Array` List of dates as Date objects
+
+
+## `dateRange.getStartDate`
+
+Get the start date as an object
+
+
+
+
+Returns `Date` Start date
+
+
+## `dateRange.getStartDate`
+
+Get a list of objects with .valid and .date members
+
+
+
+
+Returns `Array` List of dates with validity
+
+
+## `dateRange.getStartDateIndex`
+
+Get the start date index in the full list
+
+
+
+
+Returns `integer` Start date index
+
+
+## `dateRange.getValidEndDates`
+
+Get a list of dates that can be selected as the end date
+
+
+
+
+Returns `Array` List of dates that are valid end dates
+
+
+## `dateRange.getValidStartDates`
+
+Get a list of dates that can be selected as the start date
+
+
+
+
+Returns `Array` List of dates that are valid start dates
+
+
+## `dateRange.load`
+
+(re)Initializes the list of dates via a list of strings with dl
+
+### Parameters
+
+* `dl` **`Array`** An array of dates as strings
+
+
+
+
+
+## `dateRange.resetRange`
+
+Resets the start and end dates to the first and last dates in the list
+
+
+
+
+
+
+## `dateRange.setEndDate`
+
+Set the end date via the index, throws exceptions
+
+### Parameters
+
+* `index` **`integer`** Index of the desired end date
+
+
+
+
+
+## `dateRange.setRange`
+
+Sets the start and end date after resetting them
+
+
+
+
+
+
+## `dateRange.setStartDate`
+
+Set the start date via the index, throws exceptions
+
+### Parameters
+
+* `index` **`integer`** Index of the desired start date
+
+
+
+
+
--- tests/dateRangeSpec.js
+++ tests/dateRangeSpec.js
@@ -5,11 +5,134 @@ describe("dateRange", function() {
   var i = 0;
   var tmp;
   var tmpIter;
-  var validDates = [];
-  var invalidDates = [];
-  var happiness = true;
+	var dr;
+  var dateList;
 
-  it("should be happy", function() {
-    expect(happiness).toBe(true);
-  });
+	beforeEach(function() {
+		dateList = ["11/1/2014", "8/1/2014", "10/1/2014", "7/1/2014", "12/1/2014", "9/1/2014", "6/1/2014"];
+	});
+
+	afterEach(function() {
+		i = null;
+		tmp = null;
+		tmpIter = null;
+		dr = null;
+	});
+
+	describe("initialization", function() {
+
+		it("should not do anything if nothing is passed", function() {
+			dr = dateRange();
+			expect(dr.getList().length).toBe(0);
+			expect(dr.getStartDate()).toBeFalsy();
+			expect(dr.getEndDate()).toBeFalsy();
+		});
+
+		it("should sort unsorted lists of dates", function() {
+			dr = dateRange(dateList);
+			for(i = 0, tmp = dr.getList(); i < tmp.length - 1; i++) {
+				expect(tmp[i]).toBeLessThan(tmp[i + 1]);
+			}
+		});
+
+	});
+
+	describe("load()", function() {
+
+		it("should (re)initialize the list used by the object", function() {
+			dr = dateRange();
+			expect(dr.getList().length).toBe(0);
+			dr.load(dateList);
+			expect(dr.getList().length).toBe(dateList.length);
+		});
+
+		it("should convert all dates to their Date object equivalent", function() {
+			dateList = ["1/1/2014", "2/1/2014", "3/1/2014", "4/1/2014"];
+			dr = dateRange(dateList);
+			for(i = 0, tmp = dr.getList(); i < tmp.length; i++) {
+				expect(tmp[i].getTime()).toBe(new Date(dateList[i]).getTime());
+			}
+		});
+
+	});
+
+	describe("getList()", function() {
+
+		it("should return a list with length equal to the list passed in", function() {
+			dr = dateRange();
+			expect(dr.getList().length).toBe(0);
+			dr.load(dateList);
+			expect(dr.getList().length).toBe(dateList.length);
+		});
+
+	});
+
+	describe("getStartDate()", function() {
+
+		it("should return the start date", function() {
+			dateList = ["1/1/2014", "2/1/2014", "3/1/2014", "4/1/2014"];
+			dr = dateRange();
+			expect(dr.getStartDate()).toBe(undefined);
+			dr.load(dateList);
+			expect(dr.getStartDate().getTime()).toBe(new Date(dateList[0]).getTime());
+		});
+
+	});
+
+	describe("getStartDateIndex()", function() {
+
+		it("should return the start date", function() {
+			dateList = ["1/1/2014", "2/1/2014", "3/1/2014", "4/1/2014"];
+			dr = dateRange();
+			expect(dr.getStartDateIndex()).toBe(undefined);
+			dr.load(dateList);
+			expect(dr.getStartDateIndex()).toBe(0);
+		});
+
+	});
+
+	describe("getEndDate()", function() {
+
+		it("should return the end date", function() {
+			dateList = ["1/1/2014", "2/1/2014", "3/1/2014", "4/1/2014"];
+			dr = dateRange();
+			expect(dr.getEndDate()).toBe(undefined);
+			dr.load(dateList);
+			expect(dr.getEndDate().getTime()).toBe(new Date(dateList[dateList.length - 1]).getTime());
+		});
+
+	});
+
+	describe("getEndDateIndex()", function() {
+
+		it("should return the end date", function() {
+			dateList = ["1/1/2014", "2/1/2014", "3/1/2014", "4/1/2014"];
+			dr = dateRange();
+			expect(dr.getEndDateIndex()).toBe(undefined);
+			dr.load(dateList);
+			expect(dr.getEndDateIndex()).toBe(dateList.length - 1);
+		});
+
+	});
+
+	describe("setStartDate()", function() {
+
+		it("should set the start date", function() {
+			dr = dateRange(dateList);
+			dr.setStartDate(2);
+			expect(dr.getStartDate()).toBe(dr.getList()[2]);
+		});
+
+	});
+
+	describe("setEndDate()", function() {
+
+		it("should set the end date", function() {
+			dr = dateRange(dateList);
+			dr.setEndDate(2);
+			expect(dr.getEndDate()).toBe(dr.getList()[2]);
+		});
+
+	});
+
 });
