d96bd39f0d502f2f508b1bf9480a241ac7e96122
Aaron Ballard <ans.ballard@gmail.com>
2015-06-30 19:52:51 -0500

    dummy spec file, for happiness

---
+++ tests/dateRangeSpec.js
@@ -0,0 +1,13 @@
+describe("dateRange", function() { "use strict";
+  var d = dateRange; // shorthand
+  var i = 0;
+  var tmp;
+  var tmpIter;
+  var validDates = [];
+  var invalidDates = [];
+  var happiness = true;
+
+  it("should be happy", function() {
+    expect(happiness).toBe(true);
+  });
+});
